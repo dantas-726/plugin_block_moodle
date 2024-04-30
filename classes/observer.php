a55595be30ecd33b0cf557db3e0ac8424d2f9cb5
Pedro Henrique Dantas <40534089+dantas-726@users.noreply.github.com>
2024-04-30 10:43:37 -0300

    Commit message

---
+++ classes/observer.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace block_moodledev2024;
+
+/**
+ * Event observer class.
+ *
+ * @package     block_moodle_dev2024
+ * @category    event
+ * @copyright   2024 Pedro_PluginDev <pedrodevpedro.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class observer {
+
+    /**
+     * Triggered via $event.
+     *
+     * @param \core\event\user_loggedin $event The event.
+     * @return bool True on success.
+     */
+    public static function handle_login($event): bool {
+
+        // For more information about the Events API please visit {@link https://docs.moodle.org/dev/Events_API}.
+
+        return true;
+    }
+}
